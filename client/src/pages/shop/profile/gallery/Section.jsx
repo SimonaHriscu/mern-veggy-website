7c21950d486c078d9533b6cb8653b883ce50ce12
Kevin <kev.saintigny.schmidt@gmail.com>
2020-11-22 07:17:31 +0100

    test

--- client/src/pages/shop/profile/gallery/Section.jsx
+++ client/src/pages/shop/profile/gallery/Section.jsx
@@ -1,12 +1,8 @@
-import React from "react";
+import React from 'react';
 import './Section.styles.scss';
 
 const Section = ({ children }) => {
-  return (
-    <section className="farmer-profile__gallery__images">
-      {children}
-    </section>
-  );
+  return <section className="farmer-profile__gallery__images">{children}</section>;
 };
 
 export default Section;
